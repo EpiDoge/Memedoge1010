44a53667d27cd3b90a994f03c6a6482da940a807
EpiDoge <163763888+EpiDoge@users.noreply.github.com>
2026-02-18 19:02:51 -0500

    Fix canvas creation and append to document

--- lilGuy.js
+++ lilGuy.js
@@ -1,4 +1,4 @@
 let canv = document.createElement("canvas")
 canv.style.all = "position: fixed;top: 0;left: 0;right: 0;bottom: 0;width: 100%;height: 100%;background-color: rgba(0, 0, 0, 1);z-index: 10;" //covers whole screen
+document.appendChild(canv)
 let cont = canv.getContext("2d")
-
